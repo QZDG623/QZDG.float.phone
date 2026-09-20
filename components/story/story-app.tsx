51ca896a9b2ba76ba451aafa25a5bff38a652b89
QZDG623 <1704206490@qq.com>
2026-09-20 10:57:05 +0800

    revert: components/story/story-app.tsx to original export

--- components/story/story-app.tsx
+++ components/story/story-app.tsx
@@ -1,6 +1,3 @@
 "use client";
 
-import { StoryApp as BaseStoryApp } from "./story-app-base";
-import { withStoryScrollFix } from "./story-scroll-fix";
-
-export const StoryApp = withStoryScrollFix(BaseStoryApp);
+export { StoryApp } from "./story-app-base";
